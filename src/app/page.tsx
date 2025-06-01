a95422f4b8bfcbc130db3a442453fc8a90495db1
WilMay <wmayhood@outlook.com>
2025-06-01 19:08:58 -0400

    CV component complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Image from "next/image";
-import EditSection from "../../components/editSection"
+import EditSection from "../components/editSection";
 export default function Home() {
+  let features = [
+    {
+      Name: "Name",
+      Type: "text",
+    },
+    {
+      Name: "Email",
+      Type: "email",
+    },
+    {
+      Name: "Phone",
+      Type: "tel",
+    },
+  ];
   return (
-   <div>
-    Hello There
-    <EditSection />
-   </div>
+    <div>
+      Hello There
+      <EditSection id="general-info"/>
+    </div>
   );
 }
