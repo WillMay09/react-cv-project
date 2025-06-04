4918d24c5a991896adbc9f98670ae36f1f75e86f
WilMay <wmayhood@outlook.com>
2025-06-03 23:06:14 -0400

    editSection component with option to add fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,24 @@ export default function Home() {
       Type: "tel",
     },
   ];
+  let educationFeatures = [
+    {
+      Name: "School Name",
+      Type: "text",
+    },
+    {
+      Name: "Title Of Study",
+      Type: "text",
+    },
+    {
+      Name: "Date of Study",
+      Type: "date",
+    },
+  ];
   return (
     <div>
       <EditSection title="General Information" features={generalFeatures} />
-      
+      <EditSection title="Education Information" features={educationFeatures} />
     </div>
   );
 }
---
+++ src/components/editSectionWithButton.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function EditSectionWithButton({ title, features }) {
+  const [fields, setFields] = useState(features);
+
+  const handleAddField = () => {
+    const newField = { Name: `Field ${fields.length + 1}`, Type: "text" };
+    setFields([...fields, newField]);
+  };
+  return (
+    <div className="bg-gray-900 text-gray-400 flex flex-col items-center justify-center space-y-4 p-4">
+      <h2 className="text-teal-300">{title}</h2>
+      <ul>
+        {fields.map((field, index) => (
+          <li
+            key={`${fields.Name}-${index}`}
+            className="flex flex-col justify-center items-center"
+          >
+            <label>{fields.Name}</label>
+            <input
+              className="bg-gray-600 border border-gray-400 rounded py-1 px-1"
+              type={field.Type}
+              name={fields.Name}
+            />
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={handleAddField}
+        className="mt-4 px-3 py-1 bg-teal-500 text-white rounded hover:bg-teal-600 "
+      >
+        +Add Field
+      </button>
+    </div>
+  );
+}
