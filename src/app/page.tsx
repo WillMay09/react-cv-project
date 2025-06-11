b4fd50697587f164e60aa652b6229c1772946aa0
WilMay <wmayhood@outlook.com>
2025-06-11 00:24:02 -0400

    generalizing editSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,67 @@
 import Image from "next/image";
 import EditSection from "../components/editSection";
 import EditSectionWithButton from "../components/editSectionWithButton";
+import {useState} from 'react'
 export default function Home() {
-  let generalFeatures = [
-    {
-      Name: "Name",
-      Type: "text",
-    },
-    {
-      Name: "Email",
-      Type: "email",
-    },
-    {
-      Name: "Phone",
-      Type: "tel",
-    },
-  ];
-  let educationFeatures = [
-    {
-      Name: "School Name",
-      Type: "text",
-    },
-    {
-      Name: "Title Of Study",
-      Type: "text",
-    },
-    {
-      Name: "Date of Study",
-      Type: "date",
-    },
-  ];
+  // let generalFeatures = [
+  //   {
+  //     Name: "Name",
+  //     Type: "text",
+  //   },
+  //   {
+  //     Name: "Email",
+  //     Type: "email",
+  //   },
+  //   {
+  //     Name: "Phone",
+  //     Type: "tel",
+  //   },
+  // ];
+  // let educationFeatures = [
+  //   {
+  //     Name: "School Name",
+  //     Type: "text",
+  //   },
+  //   {
+  //     Name: "Title Of Study",
+  //     Type: "text",
+  //   },
+  //   {
+  //     Name: "Date of Study",
+  //     Type: "date",
+  //   },
+  // ];
 
-  let experienceFeatures = [
-    {
-      Name: "Title",
-      Type: "text",
-    },
-    {
-      Name: "Date",
-      Type: "date",
-    },
-    {
-      Name: "Company",
-      Type: "text",
-    },
-    {
-      Name: "Description",
-      Type: "text",
-    },
-  ];
+  // let experienceFeatures = [
+  //   {
+  //     Name: "Title",
+  //     Type: "text",
+  //   },
+  //   {
+  //     Name: "Date",
+  //     Type: "date",
+  //   },
+  //   {
+  //     Name: "Company",
+  //     Type: "text",
+  //   },
+  //   {
+  //     Name: "Description",
+  //     Type: "text",
+  //   },
+  // ];
+
+  const [generalInfo, setGeneralInfo] = useState({});
+  const [showResume, setShowResume] = useState(false);
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4 space-y-8">
       <h1 className="text-3xl font-bold text-teal-400">CV Builder</h1>
       <div className="flex flex-wrap min-w-[300px]">
-        <EditSection title="General Information" features={generalFeatures} />
+        {/* <EditSection title="General Information" features={generalFeatures} />
         <EditSection
           title="Education Information"
           features={educationFeatures}
-        />
+        /> */}
         <EditSectionWithButton
           title="Experience"
           features={experienceFeatures}
--- src/components/editSection.jsx
+++ src/components/editSection.jsx
@@ -1,15 +1,21 @@
-export default function EditSection({title, features}) {
+export default function EditSection({title, features, sectionData, setSectionData}) {
+  const handleChange = (e, field) =>{
+
+    setSectionData({...sectionData, [field]: e.target.value});
+  }
   return (
     <div className="bg-gray-900 text-gray-400 flex flex-col items-center gap-4 p-6 rounded-[1rem]">
       <h2 className="text-teal-300">{title}</h2>
       <ul className="w-full flex flex-col justify-center gap-2 items-center ">
         {features.map((feature) => (
-          <li key={feature.Name} className="flex flex-col justify-center items-center gap-2">
+          <li key={feature.Name} className="flex flex-col w-full">
             <label>{feature.Name} </label>
             <input
               className="bg-gray-600 border border-gray-400 rounded py-1 px-1 w-full"
               type={feature.Type}
               name={feature.Name}
+              value = {sectionData[feature.Name] || ' '}
+              onChange={(e) => handleChange(e, feature.Name)}
             ></input>
           </li>
         ))}
